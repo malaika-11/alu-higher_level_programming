f0bc8e654b769332baf441a6e2b4b04b87b39d5f
malaika-11 <m.isingizwe2@alustudent.com>
2026-02-16 07:03:28 -0800

    I am tired

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 import this
 
+zen = this.s.encode('utf-8')   # get the encoded string
+import codecs
+text = codecs.decode(zen, 'rot_13')  # decode it
+print(text, end="")  # print without adding a final newline
